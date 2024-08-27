559cc905a1cd3ad074811fa699304577893128af
POTI <potistudio2022@gmail.com>
2024-08-28 03:13:43 +0900

    ✨ wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,39 @@
 <script>
 	import Header from './Header.svelte';
 	import '../app.css';
+
+	const headerLinks = [
+		"トップページ",
+		"イベント概要",
+		"サークル参加要項",
+		"みみけん（受理確認）",
+		"チラシ、業者搬入、企業出展、取材に関して",
+		"問合せ",
+		"主催ケットコム",
+		"サークルリスト"
+	];
 </script>
 
 <div class="app">
-	<Header />
+	<!-- <Header /> -->
+	<header>
+		{#each headerLinks as link}
+			<span>[</span>
+			<a href="https://svelte.dev">{link}</a>
+			<span>]</span>
+		{/each}
+	</header>
 
 	<main>
 		<slot />
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<!-- <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p> -->
+		<span>※VOCALOID（ボーカロイド）はヤマハ株式会社の登録商標です。</span>
+		<span>※なお、バナー画像を除く、このホームページに掲載されている全ての画像・文字情報・その他の一部または全部を転載・掲載することは禁止です。</span>
+		<span>但し、ネットの他の掲示板、同人誌、ペーパー、その他で「こんなイベントがあるよー。」程度の引用なら構いません。その際に告知アドレスhttps://ketto.com/tvm/を記載（サイトの場合リンク）してください。</span>
+		<a href="https://ketto.com">個人情報保護法</a>
 	</footer>
 </div>
 
@@ -28,21 +50,18 @@
 		flex-direction: column;
 		padding: 1rem;
 		width: 100%;
-		max-width: 64rem;
+		/* max-width: 64rem; */
 		margin: 0 auto;
 		box-sizing: border-box;
+
 	}
 
 	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
+		font-size: 12px;
 
-	footer a {
-		font-weight: bold;
+		a {
+			font-weight: bold;
+		}
 	}
 
 	@media (min-width: 480px) {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,153 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import logoMain from '$lib/images/logo.png';
+	import logoSub from "$lib/images/awd.png";
+	import background from "$lib/images/background.png";
+
+
+	// import Counter from './Counter.svelte';
+	// import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>THE VOCALOID MASTER - 2024秋</title>
+	<meta name="description" content="とろサー主催" />
 </svelte:head>
 
 <section>
+	<img class="Background" src={background} alt="" />
+	<div class="Top">
+		<img class="Welcome" src={logoMain} alt="welcome" />
+		<!--! Sub Logo  -->
+		<!-- <img class="Welcome" src={logoSub} alt="welcome" /> -->
+
+		<span>ボーカロイドonly event</span>
+		<span style="font-size: 2em;">「THE VOCALOiD MASTER５７」</span>
+		<span style="font-size: 1em;"><br>略称は「<b>ボーマス</b>」です。</span>
+		<span style="font-size: 1em;">※入場は同日同会場開催で共通となります。</span>
+	</div>
+
+	<div class="Event-Desc">
+		<a href="https://twitter.com/awd_vocaloid">イベント概要</a>
+	</div>
+
+	<div class="Notification-Widget">
+		<table class="Notification-Table">
+			<tbody>
+				<tr>
+					<td>お知らせ</td>
+					<td><br><br>[2024/08/28] サイト更新しました。THE VOCALOiD MASTER５７サークル参加募集中。</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+
+	<!-- <h1>THE VOCALOID MASTER - 2024秋</h1>
+	<h2>9/21 & 9/22</h2> -->
+<!--
 	<h1>
 		<span class="welcome">
 			<picture>
-				<source srcset={welcome} type="image/webp" />
+				<source srcset={welcome} type="image/png" />
 				<img src={welcome_fallback} alt="Welcome" />
 			</picture>
 		</span>
 
-		to your new<br />SvelteKit app
-	</h1>
+		<!-- to your new<br />SvelteKit app -->
+	<!-- </h1> -->
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<!-- <Counter /> -->
 </section>
 
 <style>
 	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+		.Background {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			z-index: -1;
+		}
+
+		.Top {
+			position: relative;
+			height: auto;
+			padding-bottom: 64px;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			flex: 1;
+			border-radius: 64px;
+			background-color: #FFFFFF80;
+			box-shadow: 0 8px 8px #00000080;
+			text-shadow: 0 1px 2px #9e563a;
+
+
+
+			.Welcome {
+				width: 40%;
+				aspect-ratio: 1.75 / 1;
+			}
+
+			span {
+				position: relative;
+				top: 32px;
+				font-size: 1.2vw;
+				font-weight: 600;
+				color: #101010;
+			}
+		}
+
+		.Event-Desc {
+			position: relative;
+			top: 32px;
+
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+
+			border-radius: 64px;
+			background-color: #FFFFFF80;
+			box-shadow: 0 8px 8px #00000080;
+
+			a {
+				font-size: 32px;
+			}
+		}
+
+		.Notification-Widget {
+			position: relative;
+			top: 64px;
+			display: flex;
+			justify-content: center;
+			font-weight: 800;
+
+			.Notification-Table {
+				position: relative;
+				width: 75%;
+				font-size: 16px;
+				border: thick double #404040;
+				background-color: #F0FFFF;
+
+				td:first-child {
+					font-size: 2em;
+				}
+			}
+		}
+
 	}
 
 	h1 {
-		width: 100%;
+		font-size: 4vw;
+		font-weight: 400;
+	}
+
+	h2 {
+		position: relative;
+		top: -64px;
+		font-size: 2vw;
+		font-weight: 600;
 	}
 
 	.welcome {
